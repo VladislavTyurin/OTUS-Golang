9c54e6c2771d90647c0c400757fce218ec78cde7
Vladislav Tyurin <vst123@yandex.ru>
2023-02-07 08:51:45 +0300

    Deep refactoring. Add more tests

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,67 +12,57 @@ func isDigit(r rune) bool {
 	return r >= '0' && r <= '9'
 }
 
-func checkRepeats(current, next rune) string {
-	numOfRepeat, err := strconv.Atoi(string(next))
+func checkRepeats(prev rune, current rune) string {
+	numOfRepeat, err := strconv.Atoi(string(current))
 	if err != nil {
-		return string(current)
+		return string(prev)
 	}
-	return strings.Repeat(string(current), numOfRepeat)
+	return strings.Repeat(string(prev), numOfRepeat)
 }
 
-func checkInvalidString(current, next rune, escapedCurrent bool) bool {
-	return (current == '\\' && !isDigit(next) && next != current) ||
-		(!escapedCurrent && isDigit(current) && isDigit(next))
+func escaped(prev rune, escapedPrev bool) bool {
+	return prev == '\\' && !escapedPrev
+}
+
+func checkInvalid(prev, current rune, escapedPrev bool) bool {
+	if escaped(prev, escapedPrev) {
+		return !isDigit(current) && current != '\\'
+	}
+	return !escapedPrev && isDigit(prev) && isDigit(current)
 }
 
 func Unpack(src string) (string, error) {
 	var result strings.Builder
-	var previousRune rune
 	var currentRune rune
-	var nextRune rune
-	runes := []rune(src)
-	escapedRunes := make([]bool, len(runes))
+	var previousRune rune
+	var escapedPrevious bool
+	var i int
 
-	for i := 0; i < len(runes); i++ {
-		currentRune = runes[i]
-		switch i {
-		case 0:
-			nextRune = runes[i+1]
-			escapedRunes[i] = false
-			if isDigit(currentRune) || checkInvalidString(currentRune, nextRune, escapedRunes[i]) {
+	for i, currentRune = range src {
+		if i == 0 {
+			if isDigit(currentRune) {
+				return "", ErrInvalidString
+			}
+		} else {
+			if checkInvalid(previousRune, currentRune, escapedPrevious) {
 				return "", ErrInvalidString
 			}
 
-			if currentRune != '\\' {
-				result.WriteString(checkRepeats(currentRune, nextRune))
-			}
-
-		case len(runes) - 1:
-			previousRune = runes[i-1]
-			escapedRunes[i] = (previousRune == '\\') && !escapedRunes[i-1]
-
-			if !isDigit(currentRune) || escapedRunes[i] {
-				result.WriteRune(currentRune)
-			}
-
-		default:
-			previousRune = runes[i-1]
-			nextRune = runes[i+1]
-
-			escapedRunes[i] = (previousRune == '\\') && !escapedRunes[i-1]
-
-			if checkInvalidString(currentRune, nextRune, escapedRunes[i]) {
-				return "", ErrInvalidString
-			}
-
-			if currentRune == '\\' && !escapedRunes[i] {
-				continue
-			}
-
-			if !isDigit(currentRune) || escapedRunes[i] {
-				result.WriteString(checkRepeats(currentRune, nextRune))
+			if !isDigit(previousRune) && previousRune != '\\' || escapedPrevious {
+				result.WriteString(checkRepeats(previousRune, currentRune))
 			}
 		}
+
+		escapedPrevious = ((previousRune == '\\') && !escapedPrevious)
+		previousRune = currentRune
+	}
+
+	if !escapedPrevious && previousRune == '\\' {
+		return "", ErrInvalidString
+	}
+
+	if i > 0 && (!isDigit(previousRune) && previousRune != '\\' || escapedPrevious) {
+		result.WriteRune(previousRune)
 	}
 
 	return result.String(), nil
