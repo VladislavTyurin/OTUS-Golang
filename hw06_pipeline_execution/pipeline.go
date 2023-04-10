687f3d301d1869180bb1135e7a559f6f9edb7b00
Vladislav Tyurin <vst123@yandex.ru>
2023-04-10 18:25:39 +0300

    Completed HW6

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,39 @@ type (
 
 type Stage func(in In) (out Out)
 
+func readInDone(in In, done In) Out {
+	tmp := make(Bi)
+	go func() {
+		defer close(tmp)
+		for {
+			select {
+			case <-done:
+				return
+			case i, ok := <-in:
+				if !ok {
+					return
+				}
+				tmp <- i
+			}
+		}
+	}()
+	return tmp
+}
+
+func runPipeline(in In, done In, stages ...Stage) Out {
+	out := in
+	for _, s := range stages {
+		out = s(readInDone(out, done))
+	}
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if in == nil || len(stages) == 0 {
+		out := make(Bi)
+		close(out)
+		return out
+	}
+
+	return runPipeline(in, done, stages...)
 }
