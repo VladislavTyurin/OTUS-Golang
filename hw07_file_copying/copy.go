37d75f17f77c0a5e6d38bea55b22954f75ac6b08
Vladislav Tyurin <vst123@yandex.ru>
2023-04-20 16:42:22 +0300

    Complete hw07

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,89 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+	"path"
+
+	"github.com/cheggaaa/pb"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrEmptyPath             = errors.New("empty source or destination paths")
+	ErrEqualFromTo           = errors.New("'from' and 'to' values are equal")
+	ErrIsDirectory           = errors.New("'from' or 'to' is a directory")
 )
 
+func checkParams(fromPath, toPath string, offset int64) (os.FileInfo, error) {
+	if fromPath == "" || toPath == "" {
+		return nil, ErrEmptyPath
+	}
+
+	if fromPath == toPath {
+		return nil, ErrEqualFromTo
+	}
+
+	_, err := os.Stat(path.Dir(toPath))
+	if err != nil && os.IsNotExist(err) {
+		err = os.MkdirAll(path.Dir(toPath), 0o755)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	fileInfo, err := os.Stat(fromPath)
+	if err != nil {
+		return nil, err
+	}
+
+	if fileInfo.IsDir() {
+		return nil, ErrIsDirectory
+	}
+
+	if offset > fileInfo.Size() {
+		return nil, ErrOffsetExceedsFileSize
+	}
+	return fileInfo, nil
+}
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fileInfo, err := checkParams(fromPath, toPath, offset)
+	if err != nil {
+		return err
+	}
+
+	bar := pb.StartNew(int(fileInfo.Size()))
+	bar.Output = os.Stdout
+
+	if limit == 0 {
+		limit = fileInfo.Size()
+	}
+
+	dst, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	src, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	src.Seek(offset, io.SeekStart)
+
+	var n int64
+	n, err = io.CopyN(dst, src, limit)
+	if err != nil {
+		if !errors.Is(err, io.EOF) {
+			return err
+		}
+	}
+	bar.Add64(n)
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,17 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	defer os.RemoveAll("results")
+	require.ErrorIs(t, Copy("", "results/res.txt", 0, 100), ErrEmptyPath)
+	require.ErrorIs(t, Copy("testdata/input.txt", "", 0, 100), ErrEmptyPath)
+	require.ErrorIs(t, Copy("testdata/input.txt", "testdata/input.txt", 0, 100), ErrEqualFromTo)
+	require.ErrorIs(t, Copy("testdata/", "testdata/input.txt", 0, 100), ErrIsDirectory)
+	require.ErrorIs(t, Copy("testdata/input.txt", "results/out.txt", 10000, 100), ErrOffsetExceedsFileSize)
 }
