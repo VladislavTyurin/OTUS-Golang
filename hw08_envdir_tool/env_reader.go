5ad6ef1214c145f63dff4f98585cd1f35e904000
Vladislav Tyurin <vst123@yandex.ru>
2023-05-09 19:12:36 +0300

    Complete HW08

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"os"
+	"path"
+	"strings"
+)
+
 type Environment map[string]EnvValue
+
+var ErrInvalidName = errors.New("invalid env name")
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
 type EnvValue struct {
@@ -11,6 +23,42 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	result := make(Environment)
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+		filename := entry.Name()
+		if strings.Contains(filename, "=") {
+			return nil, fmt.Errorf("%w: %s", ErrInvalidName, filename)
+		}
+		info, err := entry.Info()
+		if err != nil {
+			return nil, err
+		}
+		if info.Size() == 0 {
+			result[filename] = EnvValue{NeedRemove: true}
+			continue
+		}
+		f, err := os.Open(path.Join(dir, filename))
+		if err != nil {
+			return nil, err
+		}
+		scanner := bufio.NewScanner(f)
+		// Not for, because we need scan only first line
+		if scanner.Scan() {
+			line := scanner.Bytes()
+			line = bytes.TrimRight(line, "\t ")
+			line = bytes.ReplaceAll(line, []byte{0x00}, []byte{'\n'})
+			result[filename] = EnvValue{
+				Value: string(line),
+			}
+		}
+	}
+	return result, nil
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,37 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("invalid name", func(t *testing.T) {
+		f, err := os.Create(path.Join("testdata", "FOO="))
+		require.NoError(t, err)
+		defer os.Remove(f.Name())
+		env, err := ReadDir("testdata")
+		require.ErrorIs(t, err, ErrInvalidName)
+		require.Nil(t, env)
+	})
+
+	t.Run("not a dir error", func(t *testing.T) {
+		_, err := ReadDir(path.Join("testdata", "echo.sh"))
+		require.Error(t, err)
+	})
+
+	t.Run("empty first line", func(t *testing.T) {
+		f, err := os.Create(path.Join("testdata", "MYFOO"))
+		require.NoError(t, err)
+		defer os.Remove(f.Name())
+
+		f.WriteString("\nvalue\n")
+		f.Close()
+		env, err := ReadDir("testdata")
+		require.NoError(t, err)
+		require.Equal(t, env["FOO"].Value, "")
+	})
 }
